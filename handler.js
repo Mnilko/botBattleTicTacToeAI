3852b80eb4177324f4a6980ba4908719b976c87f
Mezentsev Valentin <mezentsev.valentin@gmail.com>
2018-07-02 21:35:52 +0300

    Send pub/sub

--- handler.js
+++ handler.js
@@ -1,17 +1,19 @@
 const { fourbyfourAI, winningCombinations, currentTurn } = require('./fourbyfour');
 const { winningCombo } = require('./game');
 const { indexToCoordinates, coordinatesToIndex } = require('./convertor');
+const request = require('request-promise-native');
 
 const randomIntegerInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 module.exports.tictactoe = (event, context, callback) => {
-  const { board, coordinate } = JSON.parse(event.body);
+  const { board, coordinate, isAlexa = false } = JSON.parse(event.body);
   if (coordinate) board[coordinatesToIndex(coordinate)] = currentTurn(board);
 
   let boardWithAnswer;
   let newTurnCoordinate;
   let isFault;
-  if (Math.random() < 0.5) {
+  const factor = isAlexa ? 1 : 5;
+  if (Math.random() < (0.1 * factor)) {
     const emptyFieldIndexes = board.map((e, i) => e === '' ? i : null).filter(e => e);
     
     if (emptyFieldIndexes.length != 0) {
@@ -42,5 +44,16 @@ module.exports.tictactoe = (event, context, callback) => {
     }),
   };
 
-  callback(null, response);
+  var options = {
+    method: 'POST',
+    url: 'https://ps.pndsn.com/publish/pub-c-048b1453-853a-4840-8cdf-13acbba2638a/sub-c-c9df3d40-7e16-11e8-aeff-b67b4c79ce4d/0/ch1/0',
+    headers: { 'Content-Type': 'application/json' },
+    body: { board: boardWithAnswer },
+    json: true
+  };
+
+  request(options).then((resp) => {
+    console.log(resp);
+    callback(null, response)}
+  );
 };
